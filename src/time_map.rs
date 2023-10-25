e5460291183a062bd1d51e51d75b7727ea2cf331
yuvarajmadineni <yuvarajuv49@gmail.com>
2023-10-25 22:54:57 +0530

    use rust inbuilt binary search time map

--- src/time_map.rs
+++ src/time_map.rs
@@ -14,36 +14,18 @@ impl TimeMap {
     pub fn set(&mut self, key: String, value: String, timestamp: i32) {
         self.time_map
             .entry(key)
-            .and_modify(|c| c.push((value.clone(), timestamp)))
-            .or_insert([(value.clone(), timestamp)].to_vec());
+            .or_default()
+            .push((value, timestamp));
     }
 
     pub fn get(&mut self, key: String, timestamp: i32) -> String {
-        let mut next_close_val = "".to_string();
-
         if let Some(time_vec) = self.time_map.get(&key) {
-            let mut low: i32 = 0;
-            let mut high = time_vec.len() as i32 - 1;
-            let mut next_closest = i32::MIN;
-
-            while low <= high {
-                let mid = (low + high) / 2;
-
-                if timestamp == time_vec[mid as usize].1 {
-                    return time_vec[mid as usize].0.clone();
-                }
-
-                if timestamp > time_vec[mid as usize].1 {
-                    if next_closest < time_vec[mid as usize].1 {
-                        next_close_val = time_vec[mid as usize].0.clone();
-                    }
-                    next_closest = next_closest.max(time_vec[mid as usize].1);
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-            }
+            return match time_vec.binary_search_by_key(&timestamp, |&(_, time)| time) {
+                Ok(ind) => time_vec[ind].0.clone(),
+                Err(i) if i > 0 => time_vec[i - 1].0.clone(),
+                _ => "".to_string(),
+            };
         }
-        next_close_val
+        "".to_string()
     }
 }
